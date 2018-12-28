5b0db54537a1590eecfeb9df0ed8f689af34383b
szshaojiang <blackkiba@live.cn>
2018-12-28 02:31:25 +0000

    命名路由

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -2,7 +2,7 @@
   <div class="container ">
     <a class="navbar-brand" href="/">Weibo App</a>
     <ul class="navbar-nav justify-content-end">
-      <li class="nav-item"><a class="nav-link" herf="/help">帮助</a></li>
+      <li class="nav-item"><a class="nav-link" herf="{{ route('help') }}">帮助</a></li>
       <li class="nav-item"<a class="nav-link" herf="#">登录</a></li>
     </ul>
   </div>
